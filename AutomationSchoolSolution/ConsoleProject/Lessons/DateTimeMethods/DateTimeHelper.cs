0a41cd6d0e2c919d7cac9b896263875b02e2cf27
Andrii Solonchuk <asolonchuk@varonis.com>
2020-11-03 16:00:56 +0200

    GetNumberOfDaysPassedFromStartDate

---
+++ AutomationSchoolSolution/ConsoleProject/Lessons/DateTimeMethods/DateTimeHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleProject.Lessons.DateTimeMethods
+{
+    public class DateTimeHelper
+    {
+        public static double GetNumberOfDaysPassedFromStartDate(string startDate)
+        {
+            DateTime today = DateTime.Now;
+            DateTime startDateTime = DateTime.Parse(startDate);
+            TimeSpan subtractionResult = today - startDateTime;
+            return subtractionResult.TotalDays;
+        }
+    }
+}
